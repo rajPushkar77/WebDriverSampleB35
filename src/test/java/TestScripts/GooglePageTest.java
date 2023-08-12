b68e7f9c8480715eb0be5af113eae135ba66c7e0
pushk <pushk@LAPTOP-44VRAECI>
2023-08-12 19:07:55 +0530

    New Commit

--- src/test/java/TestScripts/GooglePageTest.java
+++ src/test/java/TestScripts/GooglePageTest.java
@@ -1,5 +1,7 @@
 package TestScripts;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,18 +10,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GooglePageTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver= new ChromeDriver();
 		//driver.get("https://www.google.com/");
 		driver.navigate().to("https://www.google.com/");
 		WebElement srcBox= driver.findElement(By.id("APjFqb"));
 		srcBox.sendKeys("Java Tutorial");
-		srcBox.sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		List<WebElement> items=driver.findElements(By.xpath("(//ul[@class='G43f7e'])[1]//li//descendant::div[@class='wM6W7d']"));
+		System.out.println(items.size());
+		for (int i=0;i<items.size();i++) {
+			if(items.get(i).getText().equalsIgnoreCase("Java Tutorial Pdf")) {
+				items.get(i).click();
+				break;
+			}
+		}
+		
+//		srcBox.sendKeys(Keys.ENTER);
 		String PageTitle= driver.getTitle();
 		System.out.println("Page Title....."+PageTitle);
-		driver.navigate().back();
-		driver.navigate().forward();
+//		driver.navigate().back();
+//		driver.navigate().forward();
 		driver.close();
 		
 		
