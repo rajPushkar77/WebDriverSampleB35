67c3b527d29ebcd86da8ed1f0abe13c7952b4c35
Administrator <Administrator@172.31.0.137>
2023-08-16 07:37:40 +0000

    commiting changes

--- src/test/java/TestScripts/ALertTest.java
+++ src/test/java/TestScripts/ALertTest.java
@@ -1,19 +1,25 @@
 package TestScripts;
 
+import java.time.Duration;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ALertTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver= new ChromeDriver();
+		WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(20));
 		driver.manage().window().maximize();
 		driver.navigate().to("http://demo.seleniumeasy.com/javascript-alert-box-demo.html");
 		driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
+		wait.until(ExpectedConditions.alertIsPresent());
 		Alert alert= driver.switchTo().alert();
 		System.out.println("From Simple alert..."+alert.getText());
 		alert.accept();
